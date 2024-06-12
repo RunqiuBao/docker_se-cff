d3779222cd4e12dd1595d1c90aa297bb5942fa5b
runqiu <runqiu.bao@tum.de>
2024-06-13 00:11:06 +0900

    add config for dataset gen

--- scripts/makedatasets.sh
+++ scripts/makedatasets.sh
@@ -7,9 +7,9 @@
 #     --seq_idx 0 
 
 python3 teod_makedataset.py \
-    --data_root /mnt/data/datasets/blender-vibration \
+    --data_root /home/runqiu/datasets/blender-vibration \
     --dataset_type valid \
-    --lmdb_dir /mnt/data/datasets/blender-vibration/valid/lmdb/ \
-    --view4label_dir /mnt/data/datasets/blender-vibration/valid/view4label/ \
-    --config_path /home/runqiu/code/docker_se-cff/configs/config_blendervib.yaml \
+    --lmdb_dir /home/runqiu/datasets/blender-vibration/valid/lmdb/ \
+    --view4label_dir /home/runqiu/datasets/blender-vibration/valid/view4label/ \
+    --config_path /home/runqiu/code/docker_se-cff/configs/config_datagen.yaml \
     --seq_idx 0 
