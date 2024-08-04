97d577ee4b2eb70f64995f14bd22572701818e4d
runqiu <runqiu.bao@tum.de>
2024-08-04 11:15:21 +0900

    save unitree dataset specific configs

--- scripts/distributed_main.sh
+++ scripts/distributed_main.sh
@@ -5,9 +5,9 @@ set -x
 cuda_idx='0, 2, 3'
 
 config_path=/root/code/docker_se-cff/configs/config_unitreego.yaml
-data_root=/root/data/unitree-go-dataset/
-save_root=/root/code/docker_se-cff/experiments/unitree-go/ 
+data_root=/root/data/unitree-go-dataset/objdet/
+save_root=/root/code/docker_se-cff/experiments/unitree-go/
 num_workers=3
 NUM_PROC=1
 
-CUDA_VISIBLE_DEVICES=${cuda_idx} python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=$RANDOM ../src/distributed_main.py --config_path ${config_path} --data_root ${data_root} --save_root ${save_root} --num_workers ${num_workers} #--resume_cpt /root/code/docker_se-cff/experiments/blender-vibration/weights/best.pth --only_resume_weight
+CUDA_VISIBLE_DEVICES=${cuda_idx} python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=$RANDOM ../src/distributed_main.py --config_path ${config_path} --data_root ${data_root} --save_root ${save_root} --num_workers ${num_workers} --resume_cpt /root/code/docker_se-cff/experiments/unitree-go/weights/best.pth --only_resume_weight
--- src/components/datasets/unitreego/constant.py
+++ src/components/datasets/unitreego/constant.py
@@ -1,5 +1,9 @@
 DATA_SPLIT = {
-    "train": ["train/hdr1s", "train/night1", "train/night_N0", "train/X3s"],
-    "valid": ["valid/avoid1", "valid/avoid_nogt1_s", "valid/night0", "valid/X0"],
+    "train": ["train/seq0", "train/seq1", "train/seq2", "train/seq3", "train/seq4"],
+    "valid": ["valid/seq0", "valid/seq1", "valid/seq2"],
+    "test": ["test/seq0"],  #"test/seq1", "test/seq2", "test/seq3", "test/seq4", "test/seq5", "test/seq6", "test/seq7"
     "none": [],
 }
+
+    # "train": ["train/hdr1s", "train/night1", "train/night_N0", "train/X3s", "train/avoid_nogt1_s"],
+    # "valid": ["valid/avoid1", "valid/night0", "valid/X0"],
