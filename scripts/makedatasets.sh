93f1e2360873373b53051d756f618db5579200b8
runqiu <runqiu.bao@tum.de>
2025-07-19 00:03:50 +0900

    update configs for training in dgx

--- scripts/makedatasets.sh
+++ scripts/makedatasets.sh
@@ -88,11 +88,31 @@
 #     --calib_path /root/data/unitree-go-dataset/calib.json
 
 python3 teod_makedataset.py \
-    --data_root /root/data/unitree-go-dataset/slam/ \
-    --dataset_type test \
-    --lmdb_dir /root/data/unitree-go-dataset/slam/test/lmdb/ \
-    --view4label_dir /root/data/unitree-go-dataset/slam/test/view4label/ \
-    --config_path /root/code/docker_pytorch_trainnn/configs/config_datagen_unitreego.yaml \
-    --seq_idx_toselect 2 \
-    --seq_idx 22 \
-    --calib_path /root/data/unitree-go-dataset/calib.json
+    --data_root /root/data/planer_seovs/objdet/ \
+    --dataset_type train \
+    --lmdb_dir /root/data/planer_seovs/objdet/train/lmdb/ \
+    --view4label_dir /root/data/planer_seovs/objdet/train/view4label/ \
+    --config_path /root/code/docker_pytorch_trainnn/configs/config_datagen_planerseovs.yaml \
+    --seq_idx_toselect 0 \
+    --seq_idx 0 \
+    --calib_path /root/data/planer_seovs/calib.json
+
+python3 teod_makedataset.py \
+    --data_root /root/data/planer_seovs/objdet/ \
+    --dataset_type valid \
+    --lmdb_dir /root/data/planer_seovs/objdet/valid/lmdb/ \
+    --view4label_dir /root/data/planer_seovs/objdet/valid/view4label/ \
+    --config_path /root/code/docker_pytorch_trainnn/configs/config_datagen_planerseovs.yaml \
+    --seq_idx_toselect 0 \
+    --seq_idx 0 \
+    --calib_path /root/data/planer_seovs/calib.json
+
+# python3 teod_makedataset.py \
+#     --data_root /root/data/planer_seovs/slam/ \
+#     --dataset_type test \
+#     --lmdb_dir /root/data/planer_seovs/slam/test/lmdb/ \
+#     --view4label_dir /root/data/planer_seovs/slam/test/view4label/ \
+#     --config_path /root/code/docker_pytorch_trainnn/configs/config_datagen_planerseovs.yaml \
+#     --seq_idx_toselect 0 \
+#     --seq_idx 0 \
+#     --calib_path /root/data/planer_seovs/calib.json
