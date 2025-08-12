a7853c163a9258a2b15cc5f546b0b5b2feb7e1fb
runqiu <runqiu.bao@tum.de>
2025-08-12 10:31:19 +0900

    add localtracking network

--- src/components/methods/__init__.py
+++ src/components/methods/__init__.py
@@ -1,5 +1,6 @@
 from . import base
 from . import event_stereo_object_detection
+from . import event_stereo_object_detection_and_tracking
 from . import event_stereo_object_detection_with_yolo, event_stereo_object_detection_with_yolo_pose
 from . import detr_utils
 from . import dist_utils
